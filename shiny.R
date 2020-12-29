07000df7b91cf4335124948a329e78d0906a7571
Elian Soutullo <eliansoutul@gmail.com>
2020-12-29 16:45:47 -0300

    Shiny

--- shiny.R
+++ shiny.R
@@ -10,58 +10,99 @@ ui <- fluidPage(
   titlePanel("Incendios forestales según los medios de comunicación"),
   sidebarLayout(
     sidebarPanel(
-      fluidRow(h2('Filtros'))
-    ,
-    selectInput('alcance',
-                label=h3('Seleccione alcance del medio'),
+    selectInput('output_medio',
+                label=h3('Seleccione alcance de los medios'),
                 choices = unique(data_token$scope_medio)
                 ),
     
-    uiOutput("medio_nombre")
+    helpText("Medios:"),
+    verbatimTextOutput('medios_names')
+    
     ),
     
     mainPanel( 
-      wordcloud2Output(outputId = 'graficoWord'),
-      fluidRow(column(6, plotOutput('graficoTf_idf')))
+      wordcloud2Output('graficoWord'),
+      br(),
+      fluidRow(column(6, plotOutput('graficoTf_idf')),
+               column(6, plotOutput('graficoBigrams'))),
+      fluidRow(column(4, plotOutput('graficoSentiments')),
+               column(8, plotOutput('graficoSentimentsMedios')))
       )
-    
     )
 )
 
 server <- function(input, output) {
   
-  df <- reactive({
-    df = data_token[data_token$name_medio==input$output_medio,]
-    df
+  token <- reactive({
+    token = data_token[data_token$scope_medio==input$output_medio,]
+    token
   })
   
-  ds <- reactive({
-    ds <- data_token_group[data_token_group$name_medio==input$output_medio,]
-    ds
+  tf_df <- reactive({
+    tf_df = data_token_group[data_token_group$scope_medio==input$output_medio,]
+    tf_df
   })
   
+  bigrams <- reactive({
+    bigrams = bigrams_united[bigrams_united$scope_medio==input$output_medio,]
+    bigrams
+  })
   
-  output$medio_nombre <- renderUI({
-    selectInput(inputId="output_medio", h3("Seleccionar el medio"), 
-                choices = unique(data_token[data_token$scope_medio==input$alcance,'name_medio']), 
-                selected = 0
-    )
+  nrc <- reactive({
+    nrc = sentiments_nrc[sentiments_nrc$scope_medio==input$output_medio,]
+    nrc
+  })
+  
+  bing <- reactive({
+    bing = sentiments_bing[sentiments_bing$scope_medio==input$output_medio,]
+    bing
+  })
+  
+  output$medios_names <- renderText({
+    paste0("\n",unique(token()$name_medio))
   })
   
   output$graficoWord <- renderWordcloud2({
-    
- nube <- count(df(), word, sort = T) %>% 
-      wordcloud2(size = 1, color = "random-dark", shape = "circle")
- nube
+    count(token(), word, sort = T) %>% 
+      wordcloud2(size = 1, color = "random-dark")
   })
   
-  output$graficoTf_idf = renderPlot({
+  output$graficoTf_idf <- renderPlot({
+    tf_df() %>% top_n(5) %>% 
+      ggplot(aes(x= word, y= tf_idf, fill = scope_medio)) +
+      geom_col(show.legend = FALSE) +
+      theme_classic() + 
+      coord_flip()
   
-  tf <- ds() %>% top_n(2) %>% ggplot(aes(word, tf_idf, fill = name_medio)) +
-    geom_col(show.legend = FALSE) +
-    theme_classic() +
-    coord_flip()
-  tf
+  })
+  
+  output$graficoBigrams <- renderPlot({
+    bigrams() %>%  group_by(scope_medio) %>% 
+      count(bigram, sort = TRUE) %>% 
+      top_n(10) %>% 
+      filter(n > 2) %>% 
+      ggplot(aes(x = reorder(bigram,n), y = n, fill = scope_medio)) +
+      geom_col(show.legend = F) +
+      theme_classic() +
+      coord_flip()
+  })
+  
+  output$graficoSentiments <- renderPlot({
+   bing() %>% 
+      ggplot(aes(reorder(sentimiento, n), n, fill = sentimiento)) +
+      geom_bar(stat = "identity", show.legend = FALSE) +
+      scale_fill_brewer(palette = "Spectral") +
+      theme_minimal()
+  })
+  
+  output$graficoSentimentsMedios <- renderPlot({
+    nrc() %>% 
+      ggplot(aes(reorder(sentimiento, n), n, fill = sentimiento)) +
+      geom_bar(stat = "identity", show.legend = FALSE) +
+      scale_fill_brewer(palette = "Spectral") +
+      theme_minimal() +
+      facet_wrap(~ name_medio, ncol = 2, scales = "free_x") +
+      coord_flip()
   })
 
 }
